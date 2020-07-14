aef88abaeb9c9f3598bb9be03399204d0d7b23e9
Bilal Momin <mominbilalse444@gmail.com>
2020-07-14 17:43:11 +0530

    message

--- js/main.js
+++ js/main.js
@@ -127,6 +127,249 @@
     }
   });
 
+  // flaticon popup
+	$('.image-popup-mechanic-section').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		 gallery: {
+		  enabled: true,
+		  navigateByImgClick: true,
+		  preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+		  verticalFit: true
+		},
+		zoom: {
+		  enabled: true,
+		  duration: 300 // don't foget to change the duration also in CSS
+		}
+	  });
+
+	$('.image-popup-computerized-diagnostics').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		 gallery: {
+		  enabled: true,
+		  navigateByImgClick: true,
+		  preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+		  verticalFit: true
+		},
+		zoom: {
+		  enabled: true,
+		  duration: 300 // don't foget to change the duration also in CSS
+		}
+	  });
+
+	$('.image-popup-painting-denting').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		 gallery: {
+		  enabled: true,
+		  navigateByImgClick: true,
+		  preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+		  verticalFit: true
+		},
+		zoom: {
+		  enabled: true,
+		  duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
+	$('.image-popup-electrical-section').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		 gallery: {
+		  enabled: true,
+		  navigateByImgClick: true,
+		  preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+		  verticalFit: true
+		},
+		zoom: {
+		  enabled: true,
+		  duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
+	$('.image-popup-AC-maintenance').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		 gallery: {
+		  enabled: true,
+		  navigateByImgClick: true,
+		  preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+		  verticalFit: true
+		},
+		zoom: {
+		  enabled: true,
+		  duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
+	$('.image-popup-approved-accident-quotation').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		 gallery: {
+		  enabled: true,
+		  navigateByImgClick: true,
+		  preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+		  verticalFit: true
+		},
+		zoom: {
+		  enabled: true,
+		  duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
+	$('.image-popup-providing-spare-parts').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		 gallery: {
+		  enabled: true,
+		  navigateByImgClick: true,
+		  preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+		  verticalFit: true
+		},
+		zoom: {
+		  enabled: true,
+		  duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
+	$('.image-popup-PM-check-maintenance').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		 gallery: {
+		  enabled: true,
+		  navigateByImgClick: true,
+		  preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+		  verticalFit: true
+		},
+		zoom: {
+		  enabled: true,
+		  duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
+	$('.image-popup-restoration-classic-cars').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		 gallery: {
+		  enabled: true,
+		  navigateByImgClick: true,
+		  preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+		  verticalFit: true
+		},
+		zoom: {
+		  enabled: true,
+		  duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
+	$('.image-popup-engine-repair').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		 gallery: {
+		  enabled: true,
+		  navigateByImgClick: true,
+		  preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+		  verticalFit: true
+		},
+		zoom: {
+		  enabled: true,
+		  duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
+	$('.image-popup-modification-cars').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		 gallery: {
+		  enabled: true,
+		  navigateByImgClick: true,
+		  preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+		  verticalFit: true
+		},
+		zoom: {
+		  enabled: true,
+		  duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
+	$('.image-popup-car-maintenance').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		 gallery: {
+		  enabled: true,
+		  navigateByImgClick: true,
+		  preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+		  verticalFit: true
+		},
+		zoom: {
+		  enabled: true,
+		  duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+	  
+	  
+
   $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
     disableOn: 700,
     type: 'iframe',
